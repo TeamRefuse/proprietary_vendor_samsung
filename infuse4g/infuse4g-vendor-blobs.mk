60d71a58222c52c48e36d026cb11fbfbffce8438
M. Adam Kendall <linuxbozo@gmail.com>
2011-07-24 17:44:03 -0400

    Update to Rogers dump versions

--- infuse4g/infuse4g-vendor-blobs.mk
+++ infuse4g/infuse4g-vendor-blobs.mk
@@ -23,45 +23,49 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/infuse4g/proprietary/pvrsrvinit:system/vendor/bin/pvrsrvinit \
     vendor/samsung/infuse4g/proprietary/bcm4330_mfg.bin:system/vendor/firmware/bcm4330_mfg.bin \
     vendor/samsung/infuse4g/proprietary/bcm4330_sta.bin:system/vendor/firmware/bcm4330_sta.bin \
-    vendor/samsung/infuse4g/proprietary/bcm4330_aps.bin:system/vendor/firmware/bcm4330_aps.bin \
+    vendor/samsung/infuse4g/proprietary/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
     vendor/samsung/infuse4g/proprietary/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
-    vendor/samsung/infuse4g/proprietary/melfas-touchkey.kcm.bin:system/vendor/firmware/melfas-touchkey.kcm.bin \
+    vendor/samsung/infuse4g/proprietary/melfas_touchkey.kcm.bin:system/vendor/firmware/melfas_touchkey.kcm.bin \
     vendor/samsung/infuse4g/proprietary/libGLES_android.so:system/lib/egl/libGLES_android.so \
     vendor/samsung/infuse4g/proprietary/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
     vendor/samsung/infuse4g/proprietary/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
     vendor/samsung/infuse4g/proprietary/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
     vendor/samsung/infuse4g/proprietary/gralloc.s5pc110.so:system/vendor/lib/hw/gralloc.s5pc110.so \
+	vendor/samsung/infuse4g/proprietary/libakm.so:system/vendor/lib/libakm.so \
     vendor/samsung/infuse4g/proprietary/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
     vendor/samsung/infuse4g/proprietary/libIMGegl.so:system/vendor/lib/libIMGegl.so \
     vendor/samsung/infuse4g/proprietary/libpvr2d.so:system/vendor/lib/libpvr2d.so \
     vendor/samsung/infuse4g/proprietary/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
     vendor/samsung/infuse4g/proprietary/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
     vendor/samsung/infuse4g/proprietary/libsrv_init.so:system/vendor/lib/libsrv_init.so \
-    vendor/samsung/infuse4g/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so 
+    vendor/samsung/infuse4g/proprietary/libsrv_um.so:system/vendor/lib/libsrv_um.so \
+    vendor/samsung/infuse4g/proprietary/libusc.so:system/vendor/lib/libusc.so \
+    vendor/samsung/infuse4g/proprietary/libsensor_yamaha_test.so:system/vendor/lib/libsensor_yamaha_test.so 
 
 # All the blobs necessary for infuse4g
 PRODUCT_COPY_FILES += \
+	vendor/samsung/infuse4g/proprietary/gps.conf:system/vendor/etc/gps.conf \
+    vendor/samsung/infuse4g/proprietary/gpsd:system/vendor/bin/gpsd \
+    vendor/samsung/infuse4g/proprietary/gps.xml:system/vendor/etc/gps.xml \
+    vendor/samsung/infuse4g/proprietary/gps.s5pc110.so:system/vendor/lib/hw/gps.s5pc110.so \
     vendor/samsung/infuse4g/proprietary/libril.so:system/lib/libril.so \
     vendor/samsung/infuse4g/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/infuse4g/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
     vendor/samsung/infuse4g/proprietary/rild:system/bin/rild \
-    vendor/samsung/infuse4g/proprietary/agpsd:system/vendor/bin/gpsd \
-	vendor/samsung/infuse4g/proprietary/gps.conf:system/vendor/etc/gps.conf \
+    vendor/samsung/infuse4g/proprietary/libcamera.so:system/lib/libcamera.so \
+    vendor/samsung/infuse4g/proprietary/libsensorservice.so:system/lib/libsensorservice.so \
     vendor/samsung/infuse4g/proprietary/libActionShot.so:system/lib/libActionShot.so \
     vendor/samsung/infuse4g/proprietary/libarccamera.so:system/lib/libarccamera.so \
     vendor/samsung/infuse4g/proprietary/libcamera_client.so:system/lib/libcamera_client.so \
-    vendor/samsung/infuse4g/proprietary/libcamerafirmwarejni.so:system/lib/libcamerafirmwarejni.so \
     vendor/samsung/infuse4g/proprietary/libcameraservice.so:system/lib/libcameraservice.so \
     vendor/samsung/infuse4g/proprietary/libCaMotion.so:system/lib/libCaMotion.so \
     vendor/samsung/infuse4g/proprietary/libcaps.so:system/lib/libcaps.so \
-    vendor/samsung/infuse4g/proprietary/libddc.so:system/lib/libddc.so \
-    vendor/samsung/infuse4g/proprietary/libedid.so:system/lib/libedid.so \
     vendor/samsung/infuse4g/proprietary/libPanoraMax1.so:system/lib/libPanoraMax1.so \
     vendor/samsung/infuse4g/proprietary/libPlusMe.so:system/lib/libPlusMe.so \
     vendor/samsung/infuse4g/proprietary/libs3cjpeg.so:system/lib/libs3cjpeg.so \
     vendor/samsung/infuse4g/proprietary/libseccamera.so:system/lib/libseccamera.so \
     vendor/samsung/infuse4g/proprietary/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
-    vendor/samsung/infuse4g/proprietary/libcamera.so:system/lib/libcamera.so \
+    vendor/samsung/infuse4g/proprietary/libsecjpegencoder.so:system/lib/libsecjpegencoder.so \
     vendor/samsung/infuse4g/proprietary/libtvout.so:system/lib/libtvout.so \
     vendor/samsung/infuse4g/proprietary/lib_tvoutengine.so:system/lib/lib_tvoutengine.so \
     vendor/samsung/infuse4g/proprietary/libtvoutfimc.so:system/lib/libtvoutfimc.so \
@@ -69,6 +73,5 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/infuse4g/proprietary/libtvoutservice.so:system/lib/libtvoutservice.so \
     vendor/samsung/infuse4g/proprietary/tvoutserver:system/bin/tvoutserver \
     vendor/samsung/infuse4g/proprietary/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
-    vendor/samsung/infuse4g/proprietary/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
-    vendor/samsung/infuse4g/proprietary/RS_M5LS.bin:system/firmware/BSI/RS_M5LS.bin 
+    vendor/samsung/infuse4g/proprietary/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv 
     
